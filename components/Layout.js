459b1c194416ccf38d52b3b670de34a05f63c52d
Angga Gantira <angga.gantira@gmail.com>
2021-01-27 13:19:21 +0700

    11 Bagaimana Dengan Pagination #1

--- components/Layout.js
+++ components/Layout.js
@@ -9,9 +9,14 @@ export default function Layout(props) {
                 <link rel="icon" href="https://parsinta.com/favicons/favicon-16x16.png?v=vMgGe8RqKr" />
             </Head>
             <Navbar />
-            <div className="mt-8">
+            <div className="my-8">
                 {props.children}
             </div>
+            <footer className="py-5 mt-5 border-t">
+                <div className="container">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, exercitationem neque qui error aliquam, voluptatibus cupiditate recusandae inventore pariatur saepe earum quia, nemo ducimus architecto temporibus aliquid quibusdam doloremque beatae?
+                </div>
+            </footer>
         </div>
     )
 }
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,35 +1,45 @@
+import { useRouter } from "next/router";
 import Link from "next/link";
 import Layout from "../../components/Layout";
+import { sliceUrl } from "../api/sliceUrl";
 
 export default function Index({ posts }) {
+    const router = useRouter()
+
     return (
         <Layout title="Index">
             <div className="container">
                 <div className="flex">
                     <div className="w-1/2">
-                        {posts.map((post) => {
+                        {posts.data.map((post) => {
                             return (
                                 <Link href={`/posts/${post.slug}`} key={post.id}>
-                                    <a className="block hover:bg-blue-500 hover:text-white border p-4 rounded-lg mb-5" >
+                                    <a className="block hover:bg-blue-500 hover:text-white border p-4 rounded-lg mb-5">
                                         {post.title}
                                     </a>
                                 </Link>
                             )
                         })}
+
+                        {posts.meta.links.map((link, index) => {
+                            return (
+                                <button disabled={link.url == null && 'disabled'} key={index} onClick={() => router.push(`/posts/pages/${sliceUrl(link.url)}`)} className="h-10 px-4 rounded mx-1 border" dangerouslySetInnerHTML={{ __html: link.label }} />
+                            )
+                        })}
                     </div>
                 </div>
             </div>
-        </Layout >
+        </Layout>
     )
 }
 
-export const getStaticProps = async () => {
-    const response = await fetch('http://localhost:8000/api/posts')
+export const getServerSideProps = async ({ query: { page = 1 } }) => {
+    const response = await fetch(`http://localhost:8000/api/posts?page=${page}`)
     const posts = await response.json()
 
     return {
         props: {
-            posts: posts.data
+            posts
         }
     }
 }
